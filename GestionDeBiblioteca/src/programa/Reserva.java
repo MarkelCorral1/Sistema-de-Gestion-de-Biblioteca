2e1fe191c4d932cd7a4e2858f2e2e67815f37847
ikasle <ikasle@DAW1-21.arangoya.local>
2025-01-16 12:13:28 +0100

    Creación_de_la_clase_reserva.

--- GestionDeBiblioteca/src/programa/Reserva.java
+++ GestionDeBiblioteca/src/programa/Reserva.java
@@ -1,5 +1,31 @@
 package programa;
-
+import java.util.Date;
 public class Reserva {
+    private String idReserva;
+    private Usuario usuario;
+    private Libro libro;
+    private Date fechaReserva;
 
+    public Reserva(String idReserva, Usuario usuario, Libro libro, Date fechaReserva) {
+        this.idReserva = idReserva;
+        this.usuario = usuario;
+        this.libro = libro;
+        this.fechaReserva = fechaReserva;
+    }
+
+    public String getIdReserva() {
+        return idReserva;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public Date getFechaReserva() {
+        return fechaReserva;
+    }
 }
