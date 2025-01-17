31bd4c1394440577f57b9d8a4924248ef2b252be
ikasle <ikasle@DAW1-09.arangoya.local>
2025-01-17 13:57:00 +0100

    Corregir errores de realizar prestamos y crear realizar devoluciones - Markel

--- GestionDeBiblioteca/src/programa/Princiapl.java
+++ GestionDeBiblioteca/src/programa/Princiapl.java
@@ -24,6 +24,10 @@ public class Princiapl {
 		int contadorPrestamoActual = 0;
 		Prestamo[] prestamos = new Prestamo[10000];
 		
+		int contadorIDReserva = 0;
+		int contadorReservaActual = 0;
+		Reserva[] reservas = new Reserva[10000];
+		
 		boolean seguir = true;
 		while (seguir)
 		{
@@ -42,12 +46,14 @@ public class Princiapl {
 			switch (eleccion)
 			{
 			case 1:
+				// Crear usuario
 				System.out.print("Nombre: ");
 				String nombreUsuario = sc.nextLine();
 				
 				boolean seguirRol = true;
 				
 				String rol = null;
+				int maxLibrosPrestados = 0;
 				
 				while (seguirRol)
 				{
@@ -60,23 +66,29 @@ public class Princiapl {
 					
 					if (eleccionRol == 1) {
 						rol = "estudiante";
+						maxLibrosPrestados = 3;
 						seguirRol = false;
 					} else if (eleccionRol == 2) {
 						rol = "profesor";
+						maxLibrosPrestados = 5;
 						seguirRol = false;
 					} else if (eleccionRol == 3) {
 						rol = "invitado";
+						maxLibrosPrestados = 1;
 						seguirRol = false;
 					} else {
 						System.out.println("Opcion incorrecta, elija una opcion del 1 al 3.");
 					}
 				}
 				// Crear usuario
-				usuarios[contadorUsuarioActual] = new Usuario(nombreUsuario, rol, contadorIDUsuario);
+				usuarios[contadorUsuarioActual] = new Usuario(nombreUsuario, rol, contadorIDUsuario, maxLibrosPrestados);
+				contadorUsuarioActual++;
+				contadorIDUsuario++;
 				
 				break;
 
 			case 2:
+				// Crear libro
 				System.out.print("Titulo: ");
 				String tituloLibro = sc.nextLine();
 				
@@ -92,15 +104,19 @@ public class Princiapl {
 				sc.nextLine();
 				// Crear libro
 				libros[contadorLibroActual] = new Libro(tituloLibro, autorLibro, contadorIDLibro, libroAñoPublicacion, libroCopiasDisponibles);
+				contadorLibroActual++;
+				contadorIDLibro++;
 				
 				break;
 				
 			case 3:
-				Libro libroReserva = null;
+				// Realizar prestamo
+				Libro libroPrestamo = null;
+				int nLibroEnArray = 0;
 				
-				boolean seguirLibroReserva = true;
+				boolean seguirLibroPrestamo = true;
 						
-				while (seguirLibroReserva)
+				while (seguirLibroPrestamo)
 				{
 					System.out.println("Titulo del libro: ");
 					String libroBuscar = sc.nextLine();
@@ -110,8 +126,9 @@ public class Princiapl {
 							System.out.println("Libro no encontrado, introduzca otro libro.");
 							break;
 						} else if (libros[i].getTitulo().toLowerCase().equals(libroBuscar.toLowerCase())) {
-							libroReserva = libros[i];
-							seguirLibroReserva = false;
+							libroPrestamo = libros[i];
+							seguirLibroPrestamo = false;
+							nLibroEnArray = i;
 							break;
 						}
 						
@@ -122,11 +139,12 @@ public class Princiapl {
 					}
 				}
 				
-				Usuario usuarioReserva = null;
+				Usuario usuarioPrestamo = null;
+				int nUsuarioEnArray = 0;
 				
-				boolean seguirUsuarioReserva = true;
+				boolean seguirUsuarioPrestamo = true;
 				
-				while (seguirUsuarioReserva)
+				while (seguirUsuarioPrestamo)
 				{
 					System.out.println("Nombre del usuario: ");
 					String usuarioBuscar = sc.nextLine();
@@ -136,8 +154,9 @@ public class Princiapl {
 							System.out.println("Usuario no encontrado, introduzca otro usuario.");
 							break;
 						} else if (usuarios[i].getNombre().toLowerCase().equals(usuarioBuscar.toLowerCase())) {
-							usuarioReserva = usuarios[i];
-							seguirUsuarioReserva = false;
+							usuarioPrestamo = usuarios[i];
+							seguirUsuarioPrestamo = false;
+							nUsuarioEnArray = i;
 							break;
 						}
 						
@@ -151,11 +170,80 @@ public class Princiapl {
 				System.out.print("Fecha prestamo: ");
 				String fechaPrestamo = sc.nextLine();
 				
-				prestamos[contadorPrestamoActual] = new Prestamo(contadorIDPrestamo, libroReserva, usuarioReserva, fechaPrestamo);
+				if (!usuarioPrestamo.puedeRealizarPrestamo()) {
+					System.out.println("ERROR: El usuario ha llegado al limite de libros prestados");
+				} else if (!libroPrestamo.hayCopiasDisponibles()) {
+					// Reservas
+					System.out.println("No hay libros disponibles, deseas reservarlo? (s/n)");
+					String respuestaReservar = sc.nextLine();
+					
+					if (respuestaReservar.equals("s")) {
+						reservas[contadorReservaActual] = new Reserva(contadorIDReserva, usuarioPrestamo, libroPrestamo, fechaPrestamo);
+						
+						libros[nLibroEnArray].agregarReserva(reservas[contadorReservaActual]);
+						
+						contadorReservaActual++;
+						contadorIDReserva++;
+					} else {
+						System.out.println("No se ha realizado el prestamo.");
+					}
+				} else {
+					prestamos[contadorPrestamoActual] = new Prestamo(contadorIDPrestamo, libroPrestamo, usuarioPrestamo, fechaPrestamo);
+					
+					usuarios[nUsuarioEnArray].agregarLibro(libroPrestamo);
+					
+					contadorPrestamoActual++;
+					contadorIDPrestamo++;
+				}
 				
 				break;
 				
 			case 4:
+				// Realizar devolucion
+				Usuario usuarioDevolucion = null;
+				int nUsuarioEnArray1 = 0;
+				
+				boolean seguirUsuarioDevolucion = true;
+				
+				while (seguirUsuarioDevolucion)
+				{
+					System.out.println("Nombre del usuario: ");
+					String usuarioBuscar = sc.nextLine();
+					
+					for (int i = 0; i < usuarios.length; i++) {
+						if (usuarios[i] == null) {
+							System.out.println("Usuario no encontrado, introduzca otro usuario.");
+							break;
+						} else if (usuarios[i].getNombre().toLowerCase().equals(usuarioBuscar.toLowerCase())) {
+							usuarioDevolucion = usuarios[i];
+							seguirUsuarioDevolucion = false;
+							nUsuarioEnArray1 = i;
+							break;
+						}
+						
+						if (i == usuarios.length - 1) {
+							System.out.println("Usuario no encontrado, introduzca otro usuario.");
+							break;
+						}
+					}
+				}
+				
+				System.out.println("Libros disponibles del usuario: ");
+				// Mostrar libros del usuario
+				for (int i = 0; i < usuarios[nUsuarioEnArray1].getLibrosPrestados().length; i++) {
+					if (usuarios[nUsuarioEnArray1].getLibrosPrestados()[i] != null) {
+					System.out.println("\t" + (i + 1) + ". " + usuarios[nUsuarioEnArray1].getLibrosPrestados()[i].getTitulo());
+					} else {
+						
+					}
+				}
+				System.out.println("Cual libro desea borrar (numero)");
+				
+				int libroBorrar = sc.nextInt() - 1;
+				sc.nextLine();
+				
+				// Borrar libro
+				usuarios[nUsuarioEnArray1].liberarLibro(usuarios[nUsuarioEnArray1].getLibrosPrestados()[libroBorrar]);
 				
 				break;
 				
@@ -170,6 +258,7 @@ public class Princiapl {
 			case 7:
 				seguir = false;
 				System.out.println("Saliendo del sistema de gestion de biblioteca.");
+				
 				break;
 				
 			default:
